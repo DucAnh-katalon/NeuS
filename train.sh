4512cecde3a40bcf376819a8e99456bb4681868a
aws3 <ubuntu@ip-172-20-10-115>
2023-06-28 08:41:19 +0000

    train sh

--- train.sh
+++ train.sh
@@ -2,3 +2,4 @@ mkdir public_data
 gdown --id 1zgD-uTLjO8hXcjLqelU444rwS9s9-Syg -O public_data/
 unzip public_data/data_DTU.zip -d public_data/
 pip install -r requirements.txt
+python exp_runner.py --mode train --conf confs/womask.conf --case dtu_scan105
